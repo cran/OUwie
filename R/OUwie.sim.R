079c3b594f00353c917a0a0a3718e848d29c5c4f
Jeremy M. Beaulieu <jeremy.beaulieu@yale.edu>
2011-08-19 00:00:00 +0000

    version 1.2

---
+++ R/OUwie.sim.R
@@ -0,0 +1,126 @@
+##OUwie Simulator##
+
+#written by Jeremy M. Beaulieu and Brian C. OMeara
+
+#Simulates the Hansen model of continuous characters evolving under discrete selective 
+#regimes. The input is a tree of class "phylo" that has the regimes as internal node labels 
+#and a data file the contains the regime states for each species. The trait file must be in 
+#the following order: Species names then Regime. The user must specify the parameters values
+#for each simulation (i.e. alpha, sigma.sq, theta0, theta). 
+
+##The following examples assume 2 selective regimes and different models can be specified:
+#single rate Brownian motion BM1: alpha=c(0,0); sigma.sq=c(0.9); theta0=0; theta=c(0,0)
+#two rate Brownian motion BMS: alpha=c(0,0); sigma.sq=c(0.45,.9); theta0=0; theta=c(0,0)
+#global OU (OU1): alpha=c(0.1,0.1); sigma.sq=c(0.9,0.9); theta0=1; theta=c(1,1)
+#normal OU (OUSM): alpha=c(0.1,0.1); sigma.sq=c(0.9,0.9); theta0=0; theta=c(1,2)
+#multiple sigmas (OUSMV): alpha=c(0.1,0.1); sigma.sq=c(0.45,0.9); 
+#multiple alphas (OUSMA): alpha=c(0.5,0.1); sigma.sq=c(0.9,0.9); theta0=0; theta=c(1,2)
+#multiple alphas and sigmas (OUSMVA): alpha=c(0.5,0.1); sigma.sq=c(0.45,0.9); theta0=0; theta=c(1,2)
+
+OUwie.sim <- function(phy, data, alpha, sigma.sq, theta0, theta){
+	
+	#This is annoying, but the second column has to be in there twice otherwise, error.
+	data<-data.frame(data[,2], data[,2], row.names=data[,1])
+	data<-data[phy$tip.label,]
+	
+	n=max(phy$edge[,1])
+	ntips=length(phy$tip.label)
+	
+	int.states<-factor(phy$node.label)
+	phy$node.label=as.numeric(int.states)
+	tip.states<-factor(data[,1])
+	data[,1]<-as.numeric(tip.states)
+	k<-length(levels(int.states))
+	
+	regime=matrix(rep(0,(n-1)*k), n-1, k)
+	
+	##Begins the construction of the edges matrix -- similar to the ouch format##
+	#Makes a vector of absolute times in proportion of the total length of the tree
+	branch.lengths=rep(0,(n-1))
+	branch.lengths[(ntips+1):(n-1)]=branching.times(phy)[-1]/max(branching.times(phy)[-1])
+	
+	#Obtain root state and internal node labels
+	root.state<-phy$node.label[1]
+	int.state<-phy$node.label[-1]
+	
+	#New tree matrix to be used for subsetting regimes
+	edges=cbind(c(1:(n-1)),phy$edge,phy$edge.length)
+	edges=edges[sort.list(edges[,3]),]
+	
+	edges[,4]=branch.lengths
+	mm<-c(data[,1],int.state)
+	
+	regime <- matrix(0,nrow=length(mm),ncol=length(unique(mm)))
+	#Generates an indicator matrix from the regime vector
+	for (i in 1:length(mm)) {
+		regime[i,mm[i]] <- 1 
+	}
+	#Finishes the edges matrix
+	edges=cbind(edges,regime)
+	
+	#Resort the edge matrix so that it looks like the original matrix order
+	edges=edges[sort.list(edges[,1]),]
+	
+	oldregime=root.state
+	oldtime=1
+	
+	alpha=alpha
+	sigma=sqrt(sigma.sq)
+	theta=theta
+	
+	n.cov=matrix(rep(0,n*n), n, n)
+	nodecode=matrix(c(ntips+1,1,1),1,3)
+	
+	x <- matrix(0, n, 1)
+	TIPS <- 1:ntips
+	ROOT <- ntips + 1L
+	x[ROOT,] <- theta0
+	
+	for(i in 1:length(edges[,1])){
+		anc = edges[i, 2]
+		desc = edges[i, 3]
+		
+		newregime=which(edges[i,5:(k+4)]==1)
+		current=edges[i,4]
+		if(anc%in%nodecode[,1]){
+			start=which(nodecode[,1]==anc)
+			oldtime=nodecode[start,2]
+			oldregime=nodecode[start,3]
+		}
+		else{
+			newrow=c(anc,newtime,oldregime)
+			nodecode=rbind(nodecode,newrow)
+			oldtime=newtime
+		}
+		if(oldregime==newregime){
+			newtime=current
+			x[edges[i,3],]=x[edges[i,2],]*exp(-alpha[oldregime]*(oldtime-newtime))+(theta[oldregime])*(1-exp(-alpha[oldregime]*(oldtime-newtime)))+sigma[oldregime]*rnorm(1,0,1)*sqrt((1-exp(-2*alpha[oldregime]*(oldtime-newtime)))/(2*alpha[oldregime]))
+		}
+		else{
+			newtime=current+((oldtime-current)/2)
+			epoch1=x[edges[i,2],]*exp(-alpha[oldregime]*(oldtime-newtime))+(theta[oldregime])*(1-exp(-alpha[oldregime]*(oldtime-newtime)))+sigma[oldregime]*rnorm(1,0,1)*sqrt((1-exp(-2*alpha[oldregime]*(oldtime-newtime)))/(2*alpha[oldregime]))
+			oldtime=newtime
+			newtime=current
+			x[edges[i,3],]=epoch1*exp(-alpha[newregime]*(oldtime-newtime))+(theta[newregime])*(1-exp(-alpha[newregime]*(oldtime-newtime)))+sigma[newregime]*rnorm(1,0,1)*sqrt((1-exp(-2*alpha[newregime]*(oldtime-newtime)))/(2*alpha[newregime]))
+		}
+		oldregime=newregime
+	}
+	
+	x<-round(x,8)
+	Tr.values<-cbind(phy$tip.label, data[,1], x[TIPS,])
+	Tr.values<-as.data.frame(Tr.values)
+	colnames(Tr.values)<-c("Genus_species","Reg","X")
+	write.table(Tr.values, file="sim.data", quote=FALSE, row.names=FALSE, sep="\t")
+	Tr.values<-read.delim("sim.data")
+	
+	#Cannot seem to get the dataset to work unless I output to directory, re-upload it, then delete it. Weird. X is read in as a character
+	string1<-"rm"
+	string2<- paste(string1, "sim.data", sep=" ")
+	system(string2)
+	
+	Tr.values
+	
+}
+
+
+
